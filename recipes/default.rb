d69e1204680e7934a1d469447ddfb5fbf7cfe602
Chris Wessells <chris@wessells.me>
2017-03-02 18:26:16 +0000

    updated the tests and the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+chef_require 'mongo'
+require 'mongo'
+
+users node['mongodb']['admin']['user'] do
+  password node['mongodb']['admin']['password']
+  database node['mongodb']['admin']['database']
+    action :create
+end
--- resources/users.rb
+++ resources/users.rb
@@ -3,7 +3,7 @@ property :users, kind_of: String, name_property: true
 default_action :create
 
 attribute :database, kind_of: String, required: true
-attribute :password, kind_of: String
+attribute :password, kind_of: String, required: true
 attribute :roles, kind_of: Array
 
 def initialize(*args)
@@ -16,10 +16,16 @@ action :create do
   Chef::Log.info "Creating a user #{users}"
   client = mongo_client
   begin
-    client.database.users.update(@new_resource.users,
+    if @new_resource.roles
+      client.database.users.update(@new_resource.users,
                                    password: @new_resource.password,
                                    roles: @new_resource.roles,
                                    database: @new_resource.database)
+    else
+      client.database.users.update(@new_resource.users,
+                                   password: @new_resource.password,
+                                   database: @new_resource.database)
+    end
   rescue => err
     Chef::Log.fatal "Could not add the user #{err}"
   end
